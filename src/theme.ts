bb1f38447996e6370bb55697734f584dd3194cfb
rebootedCoder2 <oborimoon@gmail.com>
2024-03-04 11:43:11 +0900

    setup

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+// const darkTheme = {
+//   textColor: "whitesmoke",
+//   backgroundColor: "#111",
+// };
+
+import { DefaultTheme } from "styled-components";
+
+// const lightTheme = {
+//   textColor: "#111",
+//   backgroundColor: "whitesmoke",
+// };
+
+export const theme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  btnColor: "tomato",
+};
